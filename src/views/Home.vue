6897d0fdd8c9668b33d7bc2e4368a24c44e5d898
danczw <devdancz@pm.me>
2021-03-01 00:27:34 +0100

    landing component first draft

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <whyNano/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import whyNano from '@/components/whyNano.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    whyNano
+  }
+}
+</script>
